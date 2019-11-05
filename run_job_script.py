3234cb67b7b7f4ce08aed54fc199b2558cfaf765
Robert Hu <robert.hu@stats.ox.ac.uk>
2019-11-05 16:37:12 +0000

    run ziz jobs and fix first public data!

--- run_job_script.py
+++ run_job_script.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     with warnings.catch_warnings(): #There are some autograd issues fyi, might wanna fix it sooner or later
         warnings.simplefilter("ignore")
         gpu = get_free_gpu(8)
-        args = vars(job_parser())
+        args = vars(job_parser().parse_args())
         print(args)
         PATH = args['PATH']
         if not os.path.exists(PATH+'all_data.pt'):
