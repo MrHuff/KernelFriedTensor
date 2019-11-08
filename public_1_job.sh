3590dbeae0ca2a8bcdd14385689cebc956fee82e
Robert Hu <robert.hu@stats.ox.ac.uk>
2019-11-08 21:51:10 +0000

    try wait

--- public_1_job.sh
+++ public_1_job.sh
@@ -1,6 +1,11 @@
-for i in 1
-  do
-    nohup python run_job_script.py --max_R 30 --PATH ./public_data/ --reg_para_a 1e-2 --reg_para_b 1e2 --fp_16 True --fused True --save_path public_job --seed $i --tensor_name all_data.pt --architecture 0 --side_info_order 1 0 2 --temporal_tag 2 --task reg --tensor_name public_data_tensor.pt --side_info_name public_article_tensor.pt public_location_tensor.pt public_time_tensor.pt &
-    sleep 10
-  done
+nohup python run_job_script.py --max_R 30 --PATH ./public_data/ --reg_para_a 1e-2 --reg_para_b 1e2 --fp_16 True --fused True --save_path public_job --seed 1 --tensor_name all_data.pt --architecture 0 --side_info_order 1 0 2 --temporal_tag 2 --task reg --tensor_name public_data_tensor.pt --side_info_name public_article_tensor.pt public_location_tensor.pt public_time_tensor.pt > public_job_1.out &
+wait
+nohup python run_job_script.py --max_R 30 --PATH ./public_data/ --reg_para_a 1e-2 --reg_para_b 1e2 --fp_16 True --fused True --save_path public_job --seed 2 --tensor_name all_data.pt --architecture 0 --side_info_order 1 0 2 --temporal_tag 2 --task reg --tensor_name public_data_tensor.pt --side_info_name public_article_tensor.pt public_location_tensor.pt public_time_tensor.pt > public_job_2.out &
+wait
+nohup python run_job_script.py --max_R 30 --PATH ./public_data/ --reg_para_a 1e-2 --reg_para_b 1e2 --fp_16 True --fused True --save_path public_job --seed 3 --tensor_name all_data.pt --architecture 0 --side_info_order 1 0 2 --temporal_tag 2 --task reg --tensor_name public_data_tensor.pt --side_info_name public_article_tensor.pt public_location_tensor.pt public_time_tensor.pt > public_job_3.out &
+wait
+nohup python run_job_script.py --max_R 30 --PATH ./public_data/ --reg_para_a 1e-2 --reg_para_b 1e2 --fp_16 True --fused True --save_path public_job --seed 4 --tensor_name all_data.pt --architecture 0 --side_info_order 1 0 2 --temporal_tag 2 --task reg --tensor_name public_data_tensor.pt --side_info_name public_article_tensor.pt public_location_tensor.pt public_time_tensor.pt > public_job_4.out &
+wait
+nohup python run_job_script.py --max_R 30 --PATH ./public_data/ --reg_para_a 1e-2 --reg_para_b 1e2 --fp_16 True --fused True --save_path public_job --seed 5 --tensor_name all_data.pt --architecture 0 --side_info_order 1 0 2 --temporal_tag 2 --task reg --tensor_name public_data_tensor.pt --side_info_name public_article_tensor.pt public_location_tensor.pt public_time_tensor.pt > public_job_5.out &
+wait
 
