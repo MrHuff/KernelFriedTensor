fc16f4587a0c19667a1b4544c314fcbbc1c2cc96
Robert Hu <robert.hu@stats.ox.ac.uk>
2019-11-25 19:43:06 +0000

    adjusted memory

--- public_arch_0_job.sh
+++ public_arch_0_job.sh
@@ -1,6 +1,6 @@
 for i in {1..5}
 do
-  nohup taskset -c 0,1 python run_job_script.py --batch_size_a 0.1 --batch_size_a 1.0 --max_lr 1e-1 --max_R 40 --PATH ./public_data/ --reg_para_a 0 --reg_para_b 100. --fp_16 True --fused True --save_path public_job --seed $i --tensor_name all_data.pt --architecture 0 --side_info_order 1 0 2 --temporal_tag 2 --task reg > public_job_$i.out &
+  nohup taskset -c 0,1 python run_job_script.py --batch_size_a 0.01 --batch_size_b 0.1 --max_lr 1e-1 --max_R 30 --PATH ./public_data/ --reg_para_a 0 --reg_para_b 100. --fp_16 True --fused True --save_path public_job --seed $i --tensor_name all_data.pt --architecture 0 --side_info_order 1 0 2 --temporal_tag 2 --task reg > public_job_$i.out &
   sleep 10
 done
 
