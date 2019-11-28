6aed8eeb97c8fd8e1395b05c550269a30a35de21
Robert Hu <robert.hu@stats.ox.ac.uk>
2019-11-28 14:38:20 +0000

    rip

--- public_arch_1_job.sh
+++ public_arch_1_job.sh
@@ -1,6 +1,6 @@
 for i in {1..5}
 do
-  nohup taskset -c 0,1 python run_job_script.py --batch_size_a 0.01 --batch_size_b 0.1 --max_lr 1e0 --max_R 150 --PATH ./public_data/ --reg_para_a 0 --reg_para_b 100. --fp_16 False --fused True --save_path public_job_arch_1 --seed $i --tensor_name all_data.pt --architecture 1 --side_info_order 1 0 2 --temporal_tag 2 --task reg > public_job_$i.out &
+  nohup taskset -c 0,1 python run_job_script.py --batch_size_a 0.01 --batch_size_b 0.1 --max_lr 1e0 --max_R 100 --PATH ./public_data/ --reg_para_a 0 --reg_para_b 100. --fp_16 False --fused True --save_path public_job_arch_1 --seed $i --tensor_name all_data.pt --architecture 1 --side_info_order 1 0 2 --temporal_tag 2 --task reg > public_job_$i.out &
   sleep 10
 done
 
