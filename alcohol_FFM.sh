9babe49a91db593b954e43b95d8eca8594bc1307
Robert Hu <robert.hu@stats.ox.ac.uk>
2019-12-01 14:20:28 +0000

    cpu affinity fix

--- alcohol_FFM.sh
+++ alcohol_FFM.sh
@@ -1,6 +1,6 @@
 for i in {1..5}
 do
-  nohup taskset -c 0,1 python run_FFM_benchmarks.py --seed $i --y_name 'Bottles Sold' --data_path alcohol_sales_parquet_FFM --SAVE_PATH ./alcohol_benchmarks > FFM_alcohol_job_$i.out &
+  nohup taskset -c $i python run_FFM_benchmarks.py --seed $i --y_name 'Bottles Sold' --data_path alcohol_sales_parquet_FFM --SAVE_PATH ./alcohol_benchmarks > FFM_alcohol_job_$i.out &
   sleep 10
 done
 
