25757e61d051da737105e87321810c563809aba9
Robert Hu <robert.hu@stats.ox.ac.uk>
2019-12-11 21:30:57 +0000

    rip

--- public_arch_0_job_dual.sh
+++ public_arch_0_job_dual.sh
@@ -4,7 +4,7 @@ do
     --batch_size_a 0.01 \
     --batch_size_b 0.10 \
     --max_lr 1e-1 \
-    --max_R 25 \
+    --max_R 30 \
     --PATH ./public_data/ \
     --reg_para_a 0 \
     --reg_para_b 5 \
