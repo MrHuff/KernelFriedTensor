5a486449e11965156d696570ded73c2452177423
MrHuff <roberthu666@gmail.com>
2019-12-12 17:35:29 +0100

    rip

---
+++ movielens_arch_0_job_ls.sh
@@ -0,0 +1,25 @@
+for i in {1..5}
+do
+  nohup taskset -c $i python run_job_script.py \
+    --batch_size_a 0.01 \
+    --batch_size_b 0.10 \
+    --max_lr 1e-1 \
+    --max_R 14 \
+    --PATH ./public_movielens_data/ \
+    --reg_para_a 0 \
+    --reg_para_b 2 \
+    --fp_16 False \
+    --fused True \
+    --save_path movielens_job_arch_0_ls \
+    --seed $i \
+    --tensor_name all_data.pt \
+    --architecture 0 \
+    --side_info_order 1 2 \
+    --temporal_tag 2 \
+    --latent_scale True \
+    --dual False \
+    --init_max 1e-1 \
+    --task reg > movielens_job_$i.out &
+  sleep 10
+done
+
--- post_process.py
+++ post_process.py
@@ -2,4 +2,4 @@ from KFT.util import post_process
 
 
 if __name__ == '__main__':
-    post_process('./public_job_arch_1_primal/','test_R2')
+    post_process('./public_job_arch_0_ls/','test_R2')
