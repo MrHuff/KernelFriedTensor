fix VI, reg and variance vs mean

KFT/KFT_fp_16.py
@@ -451,11 +451,20 @@ class variational_KFT(KFT):
         self.TT_cores = torch.nn.ModuleDict(tmp_dict)
         self.TT_cores_prime = torch.nn.ModuleDict(tmp_dict_prime)
 
-    def turn_off_mean(self):
+    def get_norms(self):
+        with torch.no_grad():
+            for i, v in self.ii.items():
+                print(torch.mean(self.TT_cores[str(i)].core_param.abs()))
+                print(torch.mean(self.TT_cores[str(i)].variance_parameters))
+                if not self.old_setup:
+                    print(torch.mean(self.TT_cores_prime[str(i)].core_param.abs()))
+                    print(torch.mean(self.TT_cores_prime[str(i)].variance_parameters))
+
+    def toggle(self,toggle):
         for i,v in self.ii.items():
-            self.TT_cores[str(i)].lock_mean()
+            self.TT_cores[str(i)].toggle_mean_var(toggle)
             if not self.old_setup:
-                self.TT_cores_prime[str(i)].lock_mean()
+                self.TT_cores_prime[str(i)].toggle_mean_var(toggle)
         return 0
 
     def collect_core_outputs_mean(self,indices):
@@ -473,7 +482,6 @@ class variational_KFT(KFT):
     def collect_core_outputs(self,indices):
         first_term = []
         second_term = []
-        debug_term = []
         total_KL = 0
         for i, v in self.ii.items():
             ix = indices[:, v]
@@ -483,7 +491,6 @@ class variational_KFT(KFT):
             base, extra, KL = tt(ix)
             first_term.append(M_prime*base)
             second_term.append((extra+base**2)*sigma_prime+(M_prime**2)*extra)
-            debug_term.append(extra)
             total_KL += KL.abs() + KL_prime
         if self.full_grad:
             group_func = self.edge_mode_collate

KFT/core_VI_components.py
@@ -11,12 +11,12 @@ torch.set_printoptions(profile="full")
 class variational_TT_component(TT_component):
     def __init__(self,r_1,n_list,r_2,cuda=None,config=None,init_scale=1.0,old_setup=False,prime=False,sub_R=1,mu_prior=0,sigma_prior=-1):
         super(variational_TT_component, self).__init__(r_1,n_list,r_2,cuda,config,init_scale,old_setup,prime=prime,sub_R=sub_R)
-        self.variance_parameters = torch.nn.Parameter(-init_scale*torch.ones(*self.shape_list),requires_grad=True)
+        self.variance_parameters = torch.nn.Parameter(-2*torch.ones(*self.shape_list),requires_grad=True)
         self.register_buffer('mu_prior',torch.tensor(mu_prior))
         self.register_buffer('sigma_prior',torch.tensor(sigma_prior))
 
     def calculate_KL(self,mean,sig):
-        KL = 0.5*((mean-self.mu_prior)**2 + sig.exp()/self.sigma_prior.exp()-1-(sig-self.sigma_prior)).mean().squeeze()
+        KL = 0.5*((mean-self.mu_prior)**2 + sig.exp()/self.sigma_prior.exp()-1-(sig-self.sigma_prior)).sum(dim=1).mean().squeeze()
         return KL
 
     def sample(self,indices):
@@ -72,12 +72,12 @@ class univariate_variational_kernel_TT(TT_kernel_component):
     def __init__(self, r_1, n_list, r_2, side_information_dict, kernel_para_dict, cuda=None,config=None,init_scale=1.0,mu_prior=0,sigma_prior=-1):
         super(univariate_variational_kernel_TT, self).__init__(r_1, n_list, r_2, side_information_dict,
                                                                  kernel_para_dict, cuda, config, init_scale)
-        self.variance_parameters = torch.nn.Parameter(-init_scale*torch.ones(*self.shape_list),requires_grad=True)
+        self.variance_parameters = torch.nn.Parameter(-2*torch.ones(*self.shape_list),requires_grad=True)
         self.register_buffer('mu_prior', torch.tensor(mu_prior))
         self.register_buffer('sigma_prior', torch.tensor(sigma_prior))
 
     def calculate_KL(self,mean,sig):
-        KL = 0.5*((mean-self.mu_prior)**2 + sig.exp()/self.sigma_prior.exp()-1-(sig-self.sigma_prior)).mean().squeeze()
+        KL = 0.5*((mean-self.mu_prior)**2 + sig.exp()/self.sigma_prior.exp()-1-(sig-self.sigma_prior)).sum(dim=1).mean().squeeze()
         return KL
 
     def forward_reparametrization(self, indices):

KFT/job_utils.py
@@ -349,6 +349,7 @@ class job_object():
             total_loss = torch.tensor(loss_list).mean().data
             Y = torch.cat(y_s, dim=0)
             y_preds = torch.cat(_y_preds)
+            print(y_preds.mean())
             if task == 'reg':
                 var_Y = Y.var()
                 ref_metric = 1. - total_loss / var_Y
@@ -441,13 +442,14 @@ class job_object():
                 torch.cuda.empty_cache()
                 print(f'val_error= {l_val}')
                 print(f'test_error= {l_test}')
+                self.model.get_norms()
             ERROR = self.train_monitor(total_loss, reg, pred_loss, y_pred,  p)
             if ERROR:
                 return ERROR
         del lrs
         return False
 
-    def outer_train_loop(self, opts, loss_func, ERROR, train_list, train_dict, warmup=False,):
+    def outer_train_loop(self, opts, loss_func, ERROR, train_list, train_dict, warmup=False,toggle=False):
         for i in train_list:
             print_garbage()
             settings = train_dict[i]
@@ -456,6 +458,8 @@ class job_object():
             f()
             print(lr)
             opt = opts[lr]
+            if self.bayesian:
+                self.model.toggle(toggle)
             ERROR = self.train_loop(opt, loss_func, warmup=warmup)
             print_garbage()
             if ERROR:
@@ -524,7 +528,10 @@ class job_object():
         self.train_config['reset'] = 1.0
         opts,loss_func,ERROR,train_list,train_dict = self.setup_runs(warmup=False)
         for i in range(self.train_config['epochs']):
-            ERROR = self.outer_train_loop(opts,loss_func,ERROR,train_list,train_dict, warmup=False)
+            ERROR = self.outer_train_loop(opts,loss_func,ERROR,train_list,train_dict, warmup=False,toggle=True)
+            if self.bayesian:
+                ERROR = self.outer_train_loop(opts, loss_func, ERROR, train_list, train_dict, warmup=False,
+                                              toggle=False)
             if ERROR:
                 if self.bayesian:
                     return -np.inf, -np.inf,-np.inf, -np.inf,-np.inf, -np.inf,-np.inf
@@ -667,31 +674,31 @@ class job_object():
             return total_cal_error,cal_dict ,predictions
     def __call__(self, parameters):
         for i in range(10):
-            try:
-                torch.cuda.empty_cache()
-                get_free_gpu(10)  # should be 0 between calls..
-                if self.bayesian:
-                    total_cal_error_val,total_cal_error_test,val_cal_dict,test_cal_dict,val_loss_final,test_loss_final,predictions = self.init_and_train(parameters)
-                    if not np.isinf(val_loss_final):
-                        torch.cuda.empty_cache()
-                        if total_cal_error_test < self.best:
-                            self.best = total_cal_error_test
-                            predictions.to_hdf(self.save_path + '/'+'VI_predictions.h5', key='VI')
-                        return {'loss': total_cal_error_val,
-                                'status': STATUS_OK,
-                                'test_loss': total_cal_error_test,
-                                'val_cal_dict':val_cal_dict,
-                                'test_cal_dict':test_cal_dict,
-                                'val_loss_final':val_loss_final,
-                                'test_loss_final':test_loss_final}
-                else:
-                    val_loss_final, test_loss_final = self.init_and_train(parameters)
-                    if not np.isinf(val_loss_final):
-                        torch.cuda.empty_cache()
-                        return {'loss': -val_loss_final, 'status': STATUS_OK, 'test_loss': -test_loss_final}
-            except Exception as e:
-                print(e)
-                torch.cuda.empty_cache()
+            # try:
+            torch.cuda.empty_cache()
+            get_free_gpu(10)  # should be 0 between calls..
+            if self.bayesian:
+                total_cal_error_val,total_cal_error_test,val_cal_dict,test_cal_dict,val_loss_final,test_loss_final,predictions = self.init_and_train(parameters)
+                if not np.isinf(val_loss_final):
+                    torch.cuda.empty_cache()
+                    if total_cal_error_test < self.best:
+                        self.best = total_cal_error_test
+                        predictions.to_hdf(self.save_path + '/'+'VI_predictions.h5', key='VI')
+                    return {'loss': total_cal_error_val,
+                            'status': STATUS_OK,
+                            'test_loss': total_cal_error_test,
+                            'val_cal_dict':val_cal_dict,
+                            'test_cal_dict':test_cal_dict,
+                            'val_loss_final':val_loss_final,
+                            'test_loss_final':test_loss_final}
+            else:
+                val_loss_final, test_loss_final = self.init_and_train(parameters)
+                if not np.isinf(val_loss_final):
+                    torch.cuda.empty_cache()
+                    return {'loss': -val_loss_final, 'status': STATUS_OK, 'test_loss': -test_loss_final}
+            # except Exception as e:
+            #     print(e)
+            #     torch.cuda.empty_cache()
         return {'loss': np.inf, 'status': STATUS_FAIL, 'test_loss': np.inf}
 
     def get_kernel_vals(self,desc):