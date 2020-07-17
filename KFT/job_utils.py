71e5bcac6b19aff5c19c6e0c9f297d0a38163e69
rhu <robert.hu@stats.ox.ac.uk>
2020-07-17 17:20:15 +0200

    KL in the ELBO

--- KFT/job_utils.py
+++ KFT/job_utils.py
@@ -367,7 +367,7 @@ class job_object():
                         loss_func = torch.nn.MSELoss()
                         pred_loss = loss_func(y_pred, y.squeeze())
                     else:
-                        pred_loss= analytical_reconstruction_error_VI(y.squeeze(),y_pred,middle_term) + reg
+                        pred_loss= analytical_reconstruction_error_VI(y.squeeze(),y_pred,middle_term)*self.train_config['sigma_y'] + reg
                 else:
                     loss_func = torch.nn.MSELoss()
                     y_pred, _ = self.model(X)
